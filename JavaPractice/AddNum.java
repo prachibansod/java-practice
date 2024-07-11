6cdd4dbef7df281dab5add248872c6c22ca56cfc
prachibansod <prachibansod1329@gmail.com>
2024-07-11 20:53:43 +0530

    java hello world program

---
+++ JavaPractice/AddNum.java
@@ -0,0 +1,9 @@
+package JavaPractice;
+
+public class AddNum {
+    public static void main(String[] args) {
+        int a=5,b=4,c;
+        c=a+b;
+        System.out.println("add 2nos"+c);
+    }
+}
